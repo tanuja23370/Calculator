ef475a0d143d67d41a0d28b86dadd3929cec8170
tanuja23370 <tanujanimbakkanavar23@gmail.com>
2025-04-29 09:49:09 -0300

    save

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+    public static void main(String[] args){
+
+        double a=12;
+        double b=27;
+        System.out.println("Addition "+(a+b));
+        System.out.println("Subtraction "+(a-b));
+        System.out.println("Multiplication "+(a*b));
+        System.out.println("Division "+(a/b));
+
+    
+    }
+}
+
---
+++ errorHandler.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const errorMessage = "error: remote origin already exists.";
+  console.error(errorMessage);
+  alert(errorMessage);
+});
